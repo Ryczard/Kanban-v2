896afac0ad36d77d9fab2cbbd25439ccd3f0184e
Konrad Ryczek <konradryczek92@gmail.com>
2018-06-13 12:17:35 +0200

    Fix bug in app.js

--- js/App.js
+++ js/App.js
@@ -19,7 +19,7 @@ $.ajax({
 function setupColumns(columns) {
 	columns.forEach(function (column) {
 		var col = new Column(column.id, column.name);
-		board.createColumn(col);
+		board.addColumn(col);
 		setupCards(col, column.cards);
 	});
 }
@@ -27,7 +27,7 @@ function setupColumns(columns) {
 function setupCards(col, cards) {
 	cards.forEach(function (card) {
 		var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-		col.createCard(cardObj);
+		col.addCard(cardObj);
 	})
 }
 
