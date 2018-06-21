18a7f98f0ed82b17c8cd270c76b09d7b1e887d8b
Konrad Ryczek <konradryczek92@gmail.com>
2018-06-21 19:58:12 +0200

    Add edit card function in card.js

--- js/Card.js
+++ js/Card.js
@@ -9,17 +9,42 @@ function Card(id, name) {
 		var card = $('<li>').addClass('card');
 		var cardDescription = $('<p>').addClass('card-description').text(self.name);
 		var cardDelete = $('<button>').addClass('btn-delete').text('x');
+		var cardEdit = $('<button>').addClass('btn-edit').text('/')
 
 		cardDelete.click(function(){
 			self.removeCard();
 		});
 
+		cardEdit.click(function(){
+      		self.editCard();
+   		 });
+
 		card.append(cardDelete)
+			.append(cardEdit)
 			.append(cardDescription);
 
 		return card;
 	}
 }
+editCard: function() {
+    var self = this;
+    var newName = prompt('Enter a new name of card',
+      this.element.children('.card-description').text());
+    var columnId = this.element.closest('.column').attr('id');
+    
+    $.ajax({
+      url: baseUrl + '/card/' + self.id,
+      method: 'PUT',
+      data: {
+        id: self.id,
+        name: newName,
+        bootcamp_kanban_column_id: columnId
+      },
+      success: function(response) {
+        self.element.children('.card-description').text(newName);
+      }
+    });
+  }
 
 Card.prototype = {
 	removeCard: function() {
